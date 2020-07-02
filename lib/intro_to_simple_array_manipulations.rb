4e36bcea069c457bc99957f1723ee3549fb24b17
Mimi Ojserkis <mimioj@gmail.com>
2020-07-02 17:53:49 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,30 @@
+def using_push(arr, element)
+  arr.push(element)
+end
+
+
+def using_unshift(arr,element)
+  arr.unshift(element)
+end
+
+
+def using_pop(arr)
+  arr.pop
+end
+
+
+def pop_with_args(arr)
+  popped_elements = arr.pop(2)
+  popped_elements
+end 
+
+
+def using_shift(arr)
+  arr.shift
+end 
+
+
+def shift_with_args(arr)
+  shifted_elements = arr.shift(2)
+  shifted_elements
+end 
